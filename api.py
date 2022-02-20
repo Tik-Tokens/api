e370412ff836664329bcf0d43e2ce780cfa15d5c
viodid <david.yunta.aller@gmail.com>
2022-02-20 20:41:30 +0000

    intial setup

--- api.py
+++ api.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, redirect, request
 import json
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return 'hello mom'
+    token = request.args.get('token')
+    if token:
+        return redirect(f'http://178.79.181.140:9999/static/{token}.jpg')
+    return 'Not your day brah'
 
 if __name__ == '__main__':
     app.run(host= '178.79.181.140' ,port=9999, debug=True)
--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,6 +1,6 @@
 import multiprocessing
 
-bind = "178.79.181.140:9999"
+bind = "178.79.181.140:11111"
 workers = multiprocessing.cpu_count() * 2 + 1
 wsgi_app = 'api:app'
 reload = True
