4792e37e1b18d20a43c162316d2e26b640f2db47
viodid <david.yunta.aller@gmail.com>
2022-02-21 18:22:59 +0000

    yes you know

--- api.py
+++ api.py
@@ -1,14 +1,25 @@
 from flask import Flask, redirect, request
 import json
+import communities
 
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
+    if request.method == 'POST':
+        # call cummunities and create entry database
+        return 'yo man this is post'
+    
     token = request.args.get('token')
+    # token number -> hash function to community // call helpers?
+    community = communities.mikros()
     if token:
-        return redirect(f'http://178.79.181.140:9999/static/{token}.jpg')
+        return json.dumps(community)
+
     return 'Not your day brah'
 
 if __name__ == '__main__':
     app.run(host= '178.79.181.140' ,port=9999, debug=True)
+    
---
+++ communities.py
@@ -0,0 +1,8 @@
+def mikros():
+    value = {
+        "img1": 'http://178.79.181.140:9999/static/mikro1.jpg',
+        "img2": 'http://178.79.181.140:9999/static/mikro2.jpg',
+        "img3": 'http://178.79.181.140:9999/static/mikro3.jpg'
+    }
+
+    return value
